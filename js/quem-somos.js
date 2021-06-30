1fc0e4fed9eb9ecc154bddc8d1cdacf07a48426c
Renan Dantas <renan@git.digitalrd.com.br>
2021-06-30 16:47:20 -0300

    0.0.1

--- js/quem-somos.js
+++ js/quem-somos.js
@@ -4,7 +4,6 @@ function fixToScroll() {
       var height = document.getElementById("quem-somos").clientHeight
       var position = $("#quem-somos").offset().top
       var range = $("#quem-somos").offset().top + height
-      console.log(scroll)
       if(scroll > position + perc(position, 50) && scroll < range - perc(range, 30)) {
         $('#sobre-nos').addClass('fixar')
         $('#sobre-nos').removeClass("fix-bottom")
